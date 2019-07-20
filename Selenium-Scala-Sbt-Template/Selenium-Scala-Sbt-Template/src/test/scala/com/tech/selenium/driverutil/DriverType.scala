87b5751a52d16e98c24fc10cae5f80a4c728c7ba
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-20 17:54:45 +0100

    Driver Types Object

---
+++ Selenium-Scala-Sbt-Template/Selenium-Scala-Sbt-Template/src/test/scala/com/tech/selenium/driverutil/DriverType.scala
@@ -0,0 +1,13 @@
+package com.tech.selenium.driverutil
+
+object DriverType extends Enumeration {
+  type EnumType = Value
+  val CHROME,
+  FIREFOX,
+  OPERA,
+  EDGE,
+  IE = Value
+
+  def withNameOpt(s: String): Option[Value] = values.find(_.toString == s)
+}
+
